10d586d21e9398bb3f15664d829a0d83d79b44ba
phymod0 <chishtiawais511@gmail.com>
2019-11-23 00:17:03 +0500

    Testing

--- lib/src/edb/test.cpp
+++ lib/src/edb/test.cpp
@@ -4,14 +4,40 @@
 #include "edb.hpp"
 
 
-template<> constexpr size_t EDBSize<int>()
+struct HelloWorld {
+        char letter1, letter2;
+        int num1, num2, num3;
+        char greeting[5];
+};
+
+
+/* TODO: implement serializer class */
+
+
+template<> void EDBSerializer<HelloWorld>(const HelloWorld& hw, char* data)
 {
-        return 4;
+}
+
+
+template<> HelloWorld EDBDeserializer<HelloWorld>(const char* data);
+{
+}
+
+
+template<> constexpr size_t EDBSize<HelloWorld>()
+{
+        return sizeof(char)
+                + sizeof(char)
+                + sizeof(int)
+                + sizeof(int)
+                + sizeof(int)
+                + 5*sizeof(char);
 }
 
 
 int main(void)
 {
-        EDB::Database<int> db("helloworld.edb");
+        EDB::Database<int> db("HelloWorld.edb");
+
         return 0;
 }
