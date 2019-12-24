454f68f09144cf8958b92f67c757c2e4d1705048
Awais Chishti <chishtiawais@hotmail.com>
2019-12-25 00:25:42 +0500

    Move template class functions to headers

--- lib/src/tree/tree.hpp
+++ lib/src/tree/tree.hpp
@@ -20,4 +20,31 @@ template <typename Node, typename Value> class Tree
 };
 
 
+template <typename Node, typename Value>
+Tree<Node, Value>& Tree<Node, Value>::operator[](const Node& node)
+{
+	auto it = children.find(node);
+	if (it == children.end()) {
+		std::shared_ptr<Tree> subtree(new Tree);
+		it = children.insert({node, subtree}).first;
+	}
+	return *(it->second);
+}
+
+
+template <typename Node, typename Value>
+bool Tree<Node, Value>::hasChild(const Node& node)
+{
+	return children.find(node) != children.end();
+}
+
+
+template <typename Node, typename Value> Value& Tree<Node, Value>::value()
+{
+	if (!maybe_value)
+		maybe_value = std::move(std::optional<Value>(Value()));
+	return *maybe_value;
+}
+
+
 #endif /* TREE */
