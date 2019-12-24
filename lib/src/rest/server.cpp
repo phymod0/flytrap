e69c30727cd6798a7135a650ab4d0a08758c981f
Awais Chishti <chishtiawais@hotmail.com>
2019-12-24 23:57:25 +0500

    Complete addEventBase

--- lib/src/rest/server.cpp
+++ lib/src/rest/server.cpp
@@ -1,4 +1,5 @@
 #include "server.hpp"
+#include "../utils/splitIterator.hpp"
 
 
 namespace REST
@@ -12,5 +13,17 @@ void Server::start() {}
 void Server::stop() {}
 
 
-void Server::addEventBase(const EventBase& evBase) {}
+void Server::addEventBase(const EventBase& evBase)
+{
+	for (const auto& route : evBase.routes) {
+		const std::string& endpoint = route.first;
+		const EventBase::RouteOps& routeOps = route.second;
+		SplitIterator<'/'> pathSegments(endpoint);
+		Tree<std::string, EventBase::RouteOps>& subhandlers = handlers;
+		for (const std::string& pathSegment : pathSegments) {
+			subhandlers = subhandlers[pathSegment];
+		}
+		subhandlers.value() = routeOps;
+	}
+}
 } // namespace REST
