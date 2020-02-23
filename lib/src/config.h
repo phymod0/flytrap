959c98aed2ba9aee6ba27ba09c00824967f7d274
phymod0 <chishtiawais@hotmail.com>
2020-02-23 19:55:07 +0500

    Default document root

--- lib/src/config.h
+++ lib/src/config.h
@@ -26,4 +26,17 @@
 #endif /* LOGGING_ENABLE */
 
 
+#ifndef DEFAULT_DOCROOT
+#ifdef _WIN32
+#define DEFAULT_DOCROOT "%UserProfile%\Desktop\flytrap\"
+#elif defined(__APPLE__) || defined(__MACH__)
+#define DEFAULT_DOCROOT "~/Desktop/flytrap/"
+#elif defined(__unix__) || defined(__linux__)
+#define DEFAULT_DOCROOT "~/flytrap/"
+#elif defined(__ANDROID__)
+/* TODO(phymod0) Define the default document root for android */
+#error Define the default document root for android
+#endif /* DEFAULT_DOCROOT */
+
+
 #endif /* FT_CONFIG */
