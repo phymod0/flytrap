3e70ceccfa79fea4f90223c9e44eb0030dbff263
phymod0 <chishtiawais@hotmail.com>
2020-06-06 15:18:05 +0500

    vector: Update documentation

--- lib/src/adt/vector/vector.h
+++ lib/src/adt/vector/vector.h
@@ -15,6 +15,8 @@
 typedef struct {
 	/** Destructor for the object pointed to by `E` */
 	void (*dtor)(void* E);
+	/** Display the object pointed to by `E` */
+	void (*print)(void* E);
 	/** Return a pointer to a copy of the object pointed to by `E` */
 	void* (*copy)(void* E);
 	/** Return non-zero iff `*A == *B` */
@@ -29,9 +31,9 @@ typedef struct {
  *
  * `ops` will be copied rather than borrowed. Operations can safely be passed as
  * `NULL` pointers as an alternative to their respective default implementations
- * (harmless no-op for `dtor`, the identity function for `copy` and the zero
- * function for the rest). `ops` itself can be `NULL` as an alternative to
- * passing all operations as `NULL` pointers. The returned vector should be
+ * (harmless no-ops for `dtor` and `print`, the identity function for `copy` and
+ * the zero function for the rest). `ops` itself can be `NULL` as an alternative
+ * to passing all operations as `NULL` pointers. The returned vector should be
  * accessed as if it were an array of `void` pointers to vector elements.
  *
  * @param ops Pointer to operations
@@ -54,14 +56,24 @@ void vector_destroy(void** vector);
 /**
  * Insert an element into a vector.
  *
- * `data` will be appended to the vector as-is and the object it points to will
- * not be copied.
+ * This function takes the address of a vector pointer as the new vector may be
+ * relocated. If successful, `element` will be appended to the new vector as-is
+ * it's underlying object will not be copied. Otherwise no change will be made.
  *
- * @param vector Pointer returned by `vector_create`
- * @param data Pointer to the data to be copied
+ * @param vector Pointer to a pointer returned by `vector_create`
+ * @param element Pointer to the element to be copied
  * @return `0` if successful, `-1` if out of memory
  */
-int vector_insert(void** vector, void* data);
+int vector_insert(void*** vector_ptr, void* element);
+
+
+/**
+ * Get the number of elements present in a vector.
+ *
+ * @param vector Pointer returned by `vector_create`
+ * @return Vector size
+ */
+size_t vector_size(void** vector);
 
 
 #endif /* VECTOR */
