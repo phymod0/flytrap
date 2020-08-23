2821563c136458f45817c2ad6c15d676403cdcf1
phymod0 <chishtiawais@hotmail.com>
2020-08-23 23:03:25 +0500

    macros: Add macros for allocation + document

--- lib/src/utils/macros.h
+++ lib/src/utils/macros.h
@@ -2,7 +2,14 @@
 #define FT_UTILS_MACROS
 
 
+/** Use to suppress unused variable warnings */
 #define FT_UNUSED(x) ((void)(x))
+
+/** Use to allocate a single instance of an object type */
+#define FT_ALLOC(x) ((x) = malloc(sizeof *(x)))
+
+/** Use to allocate multiple instances of an object type */
+#define FT_VALLOC(x, n) ((x) = malloc((sizeof *(x)) * (n)))
 
 
 #endif /* FT_UTILS_MACROS */
